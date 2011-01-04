69a7128eaf2b3b2bdf65c56e0c724501b362c0d2
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2011-01-04 13:11:27 +0100

    Save and show last displayed slide on app restart.

--- iOS/Classes/SJAppDelegate.m
+++ iOS/Classes/SJAppDelegate.m
@@ -15,6 +15,8 @@
 
 #import "SJClient.h"
 
+#import "SJSlide.h"
+
 #import "SJSlideSync.h"
 #import "SJPresentationSync.h"
 #import "SJPointSync.h"
@@ -24,6 +26,8 @@
 
 @property(nonatomic, retain) SJSyncCoordinator* syncCoordinator;
 @property(nonatomic, retain) SJLiveSyncController* liveSyncController;
+
+- (void) saveCurrentSlideIdentifier;
 
 @end
 
@@ -65,9 +69,36 @@
 	livePane.liveSyncController = self.liveSyncController;
 	livePane.managedObjectContext = moc;
 	
+	NSString* slideIdentifier = [[NSUserDefaults standardUserDefaults] objectForKey:@"SJLastDisplayedSlideURIIdentifier"];
+	NSURL* slideIdentifierURL = nil; 
+	NSManagedObjectID* slideIdentifierObject = nil;
+
+	if (slideIdentifier)
+		slideIdentifierURL = [NSURL URLWithString:slideIdentifier];
+		
+	if (slideIdentifierURL)
+		slideIdentifierObject = [self.persistentStoreCoordinator managedObjectIDForURIRepresentation:slideIdentifierURL];
+	
+	if (slideIdentifierObject) {
+		id s = [self.managedObjectContext objectWithID:slideIdentifierObject];
+		if (s && [s isKindOfClass:[SJSlide class]])
+			livePane.displayedSlide = s;
+	}
+	
     [window makeKeyAndVisible];
     
     return YES;
+}
+
+- (void) saveCurrentSlideIdentifier;
+{	
+	SJSlide* s = livePane.displayedSlide;
+	NSManagedObjectID* ident = [s objectID];
+	
+	if (![ident isTemporaryID]) {
+		NSString* slideIdentifier = [[ident URIRepresentation] absoluteString];
+		[[NSUserDefaults standardUserDefaults] setObject:slideIdentifier forKey:@"SJLastDisplayedSlideURIIdentifier"];
+	}
 }
 
 - (void) userDefaultsDidChange:(NSNotificationCenter*) nc;
@@ -76,42 +107,16 @@
 		abort();
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
+- (void) applicationDidEnterBackground:(UIApplication*) application;
+{
     [self saveContext];
+	[self saveCurrentSlideIdentifier];
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    /*
-     Called as part of the transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
-     */
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-
-/**
- applicationWillTerminate: saves changes in the application's managed object context before the application terminates.
- */
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void) applicationWillTerminate:(UIApplication *) application;
+{
     [self saveContext];
+	[self saveCurrentSlideIdentifier];
 }
 
 
